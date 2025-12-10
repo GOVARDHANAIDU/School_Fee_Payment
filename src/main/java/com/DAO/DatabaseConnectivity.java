6211919acca3e6cbf481e23b501866f1f7561e37
Your Name <govardhannaiduece@gmail.com>
2025-12-11 02:35:31 +0530

    Whole attendance dashboard

--- src/main/java/com/DAO/DatabaseConnectivity.java
+++ src/main/java/com/DAO/DatabaseConnectivity.java
@@ -5,17 +5,17 @@ import java.sql.DriverManager;
 
 public class DatabaseConnectivity {
 
-//	private static final String URL =
-//		    "jdbc:mysql://trolley.proxy.rlwy.net:49592/school_data?useSSL=false&allowPublicKeyRetrieval=true";
-//		private static final String USER = "root";
-//		private static final String PASSWORD = "IIoeacGMfpglDLjgmSkwWIQoajFikXvz";
-//		private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-//
+	private static final String URL =
+		    "jdbc:mysql://trolley.proxy.rlwy.net:49592/school_data?useSSL=false&allowPublicKeyRetrieval=true";
+		private static final String USER = "root";
+		private static final String PASSWORD = "IIoeacGMfpglDLjgmSkwWIQoajFikXvz";
+		private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
 	
-		  private static final String URL = "jdbc:mysql://localhost/school_data";
-		  private static final String USER = "root";
-		  private static final String PASSWORD = "W7301@jqir#";
-		  private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+//		  private static final String URL = "jdbc:mysql://localhost/school_data";
+//		  private static final String USER = "root";
+//		  private static final String PASSWORD = "W7301@jqir#";
+//		  private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
     public static Connection getConnection() {
         Connection conn = null;
